28a54667260d187425b3ad296ab4346412b03ff0
michal <michalwozniak@live.ca>
2015-08-27 16:14:38 -0400

    formatting

--- temp-monitor.ino
+++ temp-monitor.ino
@@ -12,14 +12,14 @@
 /*
 
 
-Colors : some are inverse while using a different lcd ( ebay imitation ) 
+Colors : some are inverse while using a different lcd ( ebay imitation )
 ST7735_BLACK     0x0000
 ST7735_BLUE      0x001F
 ST7735_RED       0xF800
 ST7735_GREEN     0x07E0
 ST7735_CYAN      0x07FF
 ST7735_MAGENTA   0xF81F
-ST7735_YELLOW    0xFFE0 
+ST7735_YELLOW    0xFFE0
 ST7735_WHITE     0xFFFF
 
 
@@ -28,7 +28,7 @@ ST7735_WHITE     0xFFFF
 
 #define cs   A2
 #define dc   D6
-#define rst  D2 
+#define rst  D2
 
 Adafruit_ST7735 tft = Adafruit_ST7735(cs, dc, rst); // hardware spi
 
@@ -46,43 +46,43 @@ int ow = D0;    // put the onewire bus on D0
 OneWire  ds(ow);  // a 1 - 4.7K resistor to 3.3v is necessary
 
 void setup(void) {
-    
-    //screen
-	tft.initG();
-     
-	tft.fillScreen(ST7735_BLACK);
 
-    tft.setFont(ARIAL_8);
-    tft.setTextColor(ST7735_BLUE); //red and blue are inverse in some imitation lcd 
-    tft.setTextSize(2);
-    
-    tft.println("temperature"); //Temperature Weather 
-    tft.drawFastHLine( 0, 25, tft.width()-1, ST7735_WHITE );
+  //screen
+  tft.initG();
 
-    
-    //cloud variable
-    Spark.variable("Temperature", &sparkCelcius, DOUBLE);
+  tft.fillScreen(ST7735_BLACK);
 
-    
-    //for debugging
-    Serial.begin(57600);  // local hardware test only
-    
-   
-    
+  tft.setFont(ARIAL_8);
+  tft.setTextColor(ST7735_BLUE); //red and blue are inverse in some imitation lcd
+  tft.setTextSize(2);
+
+  tft.println("temperature"); //Temperature Weather
+  tft.drawFastHLine( 0, 25, tft.width() - 1, ST7735_WHITE );
+
+
+  //cloud variable
+  Spark.variable("Temperature", &sparkCelcius, DOUBLE);
+
+
+  //for debugging
+  Serial.begin(57600);  // local hardware test only
+
+
+
 }
 
 
 
 void loop(void) {
-    
-    
+
+
   byte i;
   byte present = 0;
   byte type_s;
   byte data[12];
   byte addr[8];
- 
-  
+
+
   /*
   if ( !ds.search(addr)) {
     Serial.println("No more addresses.");
@@ -91,7 +91,7 @@ void loop(void) {
     delay(250);
     return;
   }
-  
+
   Serial.print("ROM =");
   for( i = 0; i < 8; i++) {
     Serial.write(' ');
@@ -103,7 +103,7 @@ void loop(void) {
       return;
   }
   Serial.println();
- 
+
   // the first ROM byte indicates which chip
   // includes debug output of chip type
   switch (addr[0]) {
@@ -126,30 +126,30 @@ void loop(void) {
     default:
       Serial.println("Device is not a DS18x20/DS1822/DS2438 device. Skipping...");
       return;
-  } 
+  }
 
     */
-    // addr = 
-    ds.search(addr);
-    type_s =0;
-    
+  // addr =
+  ds.search(addr);
+  type_s = 0;
+
   ds.reset();
   ds.select(addr);      // Just do one at a time for testing
-                        // change to skip if you already have a list of addresses
-                        // then loop through them below for reading
-                        
+  // change to skip if you already have a list of addresses
+  // then loop through them below for reading
+
   ds.write(0x44);        // start conversion, with parasite power on at the end
-  
+
   delay(25);     // maybe 750ms is enough, maybe not, I'm shooting for 1 reading per second
-                    // prob should set to min reliable and check the timer for 1 second intervals
-                    // but that's a little fancy for test code
-             
-				// delay must be around 800ms-1000ms so it doesn't give error like -0.006 every 10 reading
-				// in this project, I use a big delay between print and delete (output to screen -> line 232)
+  // prob should set to min reliable and check the timer for 1 second intervals
+  // but that's a little fancy for test code
+
+  // delay must be around 800ms-1000ms so it doesn't give error like -0.006 every 10 reading
+  // in this project, I use a big delay between print and delete (output to screen -> line 232)
   // we might do a ds.depower() here, but the reset will take care of it.
-  
+
   present = ds.reset();
-  ds.select(addr);    
+  ds.select(addr);
   ds.write(0xBE, 0);         // Read Scratchpad 0
 
   Serial.print("  Data = ");
@@ -170,15 +170,15 @@ void loop(void) {
   // even when compiled on a 32 bit processor.
   int16_t raw = (data[1] << 8) | data[0];
   if (type_s) {
-    if (type_s==1) {    // DS18S20
+    if (type_s == 1) {  // DS18S20
       raw = raw << 3; // 9 bit resolution default
       if (data[7] == 0x10) {
         // "count remain" gives full 12 bit resolution
         raw = (raw & 0xFFF0) + 12 - data[6];
       }
       celsius = (float)raw / 16.0;
-    }else{ // type_s==2 for DS2438
-      if (data[2] > 127) data[2]=0;
+    } else { // type_s==2 for DS2438
+      if (data[2] > 127) data[2] = 0;
       data[1] = data[1] >> 3;
       celsius = (float)data[2] + ((float)data[1] * .03125);
     }
@@ -190,20 +190,20 @@ void loop(void) {
     else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
     //// default is 12 bit resolution, 750 ms conversion time
     celsius = (float)raw / 16.0;
-    
+
     //for spark cloud
     sparkCelcius = (double)celsius;
   }
   fahrenheit = celsius * 1.8 + 32.0;
   // end of test code
-  
+
   // debug output
   Serial.print("  Temperature = ");
   Serial.print(celsius);
   Serial.print(" Celsius, ");
   Serial.print(fahrenheit);
   Serial.println(" Fahrenheit");
-  
+
   //test screen
   // Read the value of the sensor on A0
   String TempC = String(celsius);
@@ -224,16 +224,15 @@ void loop(void) {
   tft.println("C");
   tft.setCursor(10, 100);
   tft.println(fahrenheitPrint);
-  tft.setCursor(80,100);
+  tft.setCursor(80, 100);
   tft.println("F");
   // wait for a moment
   delay(1000);
   // erase the text you just wrote
-  //tft.stroke(0, 0, 0);
-    tft.setTextColor(ST7735_BLACK);
-    tft.setCursor(10, 50);
-    tft.println(celsiusPrint);
-    tft.setCursor(10, 100);
-    tft.println(fahrenheitPrint);
-  
+  tft.setTextColor(ST7735_BLACK);
+  tft.setCursor(10, 50);
+  tft.println(celsiusPrint);
+  tft.setCursor(10, 100);
+  tft.println(fahrenheitPrint);
+
 }
